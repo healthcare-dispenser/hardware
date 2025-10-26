f1fead25229b4dac53c73d64a21a607cc0c582c2
DahyeLee <nan0462@naver.com>
2025-10-27 02:29:29 +0900

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -2,6 +2,7 @@
 import json
 import logging
 import paho.mqtt.client as mqtt
+
 from common import (
     DEVICE_UUID,
     topics,
@@ -18,8 +19,8 @@ if not log.handlers:
 
 def get_client() -> mqtt.Client:
     """
-    MQTT 클라이언트 객체만 만들어서 돌려준다.
-    (connect는 listener.py 쪽에서 한다)
+    MQTT 클라이언트 객체만 생성해서 돌려준다.
+    connect()는 listener에서 한다.
     """
     client = mqtt.Client(
         client_id=DEVICE_UUID,
@@ -28,9 +29,6 @@ def get_client() -> mqtt.Client:
     return client
 
 def publish_register(client: mqtt.Client):
-    """
-    라즈베리파이가 부팅/연결 직후 '나 여기 있어'라고 서버에 알려주는 등록 메시지 발행
-    """
     t = topics()
     payload = build_register_payload()
     client.publish(t["pub_register"], json.dumps(payload), qos=1)
@@ -38,8 +36,7 @@ def publish_register(client: mqtt.Client):
 
 def publish_command_response(client: mqtt.Client, command_uuid: str, status: str):
     """
-    펌프 동작 결과를 서버로 알려줌
-    status: 'SUCCESS' 또는 'FAIL'
+    펌프 동작 결과를 서버로 알려준다.
     """
     t = topics()
     payload = build_command_response(command_uuid, status)
